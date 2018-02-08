85ad9c2a53b08c1427a8bc3a08234a99a3603a62
Aldrin Navarro <aldrin@channelfix.com>
2018-02-08 19:00:33 +0800

    Working basic ws

---
+++ server.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'secret!'
+app.config['debug'] = True
+socketio = SocketIO(app)
+
+
+@socketio.on('message')
+def handle_message(message):
+    send(message)
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    emit('my response', json)
+
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
