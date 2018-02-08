fcbce250d72a6adcf36ddd9da493f4f372c710f1
Aldrin Navarro <aldrin@channelfix.com>
2018-02-08 20:33:07 +0800

    Complete enemy relay

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+
 import json
+import time
+from uuid import uuid4
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
@@ -7,26 +10,56 @@ app.config['SECRET_KEY'] = 'secret!'
 app.config['debug'] = True
 socketio = SocketIO(app)
 
-
-@socketio.on('message')
-def handle_message(message):
-    send(message)
+players = []
 
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    emit('my response', json)
+@socketio.on('game:register:request')
+def register_player(request):
+    if len(players) == 2:  # noqa stupid
+        return
+    player_id = str(uuid4())
+    players.append(player_id)
+    emit('game:register:success', {'player_id': player_id})
+    print 'In game: ', len(players)
+
+
+@socketio.on('game:join')
+def join_player(request):
+    player_id = request.get('playerid')
+    if player_id not in players:
+        players.append(player_id)
+    emit('game:join:success', {'player_id': player_id})
+    print 'In game: ', len(players)
+
+
+@socketio.on('game:request_opponent')
+def request_opponent(request):
+    player_id = request.get('playerid')
+    opponent = get_opponent(player_id)
+    if opponent:
+        emit('game:request_opponent:ready', {'opponent_id': opponent})
+    else:
+        emit('game:request_opponent:fail', {})
+
+
+def get_opponent(player_id):
+    try:
+        if players.index(player_id) == 0:
+            return players[1]
+        return players[0]
+    except (IndexError, ValueError):
+        return None
 
 
 @socketio.on('message:shoot')
 def handle_relay(request):
-    player = request.get('player')
-    opponent = 'player2'
-    if player == 'player2':
-        opponent = 'player1'
+    # player = request.get('player')
+    opponent = request.get('opponent')
     emit(
-        'message:pass:%s' % opponent,
-        json.dumps({'message': request.get('message')}))
+        'message:pass',
+        {'message': request.get('message'),
+         'target': opponent},
+        broadcast=True)
 
 
 @app.route('/')
